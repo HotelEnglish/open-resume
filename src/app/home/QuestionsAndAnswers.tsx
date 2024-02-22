185755f8d9251b6549ebc57cd4443813e0996b01
Miro <45430033+HotelEnglish@users.noreply.github.com>
2024-02-22 16:50:10 +0800

    Update QuestionsAndAnswers.tsx

--- src/app/home/QuestionsAndAnswers.tsx
+++ src/app/home/QuestionsAndAnswers.tsx
@@ -2,120 +2,64 @@ import { Link } from "components/documentation";
 
 const QAS = [
   {
-    question:
-      "Q1. What is a resume builder? Why resume builder is better than resume template doc?",
+    question: "Q1. 什么是简历生成器？为什么简历生成器比简历模板文档更好用？",
     answer: (
       <>
         <p>
-          There are two ways to create a resume today. One option is to use a
-          resume template, such as an office/google doc, and customize it
-          according to your needs. The other option is to use a resume builder,
-          an online tool that allows you to input your information and
-          automatically generates a resume for you.
+          当今创建简历有两种方式。一种是使用简历模板，如Office/Google文档，然后根据自身需求进行定制。另一种则是使用在线简历生成器，允许你输入信息并自动为你生成一份简历。
         </p>
         <p>
-          Using a resume template requires manual formatting work, like copying
-          and pasting text sections and adjusting spacing, which can be
-          time-consuming and error-prone. It is easy to run into formatting
-          issues, such as using different bullet points or font styles after
-          copying and pasting. On the other hand, a resume builder like
-          OpenResume saves time and prevents formatting mistakes by
-          automatically formatting the resume. It also offers the convenience of
-          easily changing font types or sizes with a simple click. In summary, a
-          resume builder is easier to use compared to a resume template.
+          使用简历模板需要手动调整格式，例如复制粘贴文本部分和调整间距，这可能会非常耗时且容易出错。在复制粘贴后很容易出现格式问题，比如使用不同的项目符号或字体样式。相反，像OpenResume这样的简历生成器可以节省时间并防止格式错误，因为它会自动格式化简历。此外，它还提供了方便的更改字体类型或大小的功能，只需轻轻一点即可。总之，与简历模板相比，简历生成器更容易使用。
         </p>
       </>
     ),
   },
   {
-    question:
-      "Q2. What uniquely sets OpenResume apart from other resume builders and templates?",
+    question: "Q2. OpenResume与其他简历生成器和模板相比，有哪些独特之处？",
     answer: (
       <>
         <p>
-          Other than OpenResume, there are some great free resume builders out
-          there, e.g. <Link href="https://rxresu.me/">Reactive Resume</Link>,{" "}
-          <Link href="https://flowcv.com/">FlowCV</Link>. However, OpenResume
-          stands out with 2 distinctive features:
+          除了OpenResume之外，市面上也有一些优秀的免费简历生成器，例如<Link href="https://rxresu.me/">Reactive Resume</Link>,{" "}
+          <Link href="https://flowcv.com/">FlowCV</Link>。然而，OpenResume凭借两个独特的特点脱颖而出：
         </p>{" "}
         <p>
           <span className="font-semibold">
-            1. OpenResume is designed specifically for the U.S. job market and
-            best practices.
+            1. OpenResume专为美国就业市场及最佳实践设计。
           </span>
           <br />
-          Unlike other resume builders that target a global audience and offer
-          many customization options, OpenResume intentionally only offers
-          options that are aligned with U.S. best practices. For example, it
-          excludes the option to add a profile picture to avoid bias and
-          discrimination. It offers only the core sections, e.g. profile, work
-          experience, education, and skills, while omitting unnecessary sections
-          like references. Additionally, OpenResume only offers a top down
-          single column resume design as opposed to two column design, because
-          single column design works best for AST. <br />{" "}
+          不同于其他面向全球用户、提供众多自定义选项的简历生成器，OpenResume有意只提供符合美国最佳实践的选项。例如，为了避免偏见和歧视，它不提供添加个人头像的选项。它仅提供核心部分，如个人信息、工作经验、教育背景和技能，而不包括诸如推荐人等不必要的部分。另外，OpenResume只提供自上而下的单栏式简历设计，而非两栏设计，因为单栏设计最适合ATS解析。 <br />{" "}
         </p>
         <p>
           <span className="font-semibold">
-            2. OpenResume is super privacy focus.
+            2. OpenResume极度注重隐私保护。
           </span>{" "}
           <br />
-          While other resume builders may require email sign up and store user
-          data in their databases, OpenResume believes that resume data should
-          remain private and accessible only on user’s local machine. Therefore,
-          OpenResume doesn’t require sign up to use the app, and all inputted
-          data is stored in user’s browser that only user has access to.
+          其他简历生成器可能要求邮箱注册并将用户数据存储在他们的数据库中，但OpenResume认为简历数据应保持私密，仅限用户本地设备访问。因此，OpenResume无需注册即可使用该应用，所有输入的数据都存储在用户的浏览器中，只有用户自己能够访问。
         </p>
       </>
     ),
   },
   {
-    question: "Q3. Who created OpenResume and why?",
+    question: "Q3. OpenResume是谁创建的？又是出于什么原因创建的？",
     answer: (
       <p>
-        OpenResume was created by{" "}
-        <Link href="https://github.com/xitanggg">Xitang Zhao</Link> and designed
-        by <Link href="https://www.linkedin.com/in/imzhi">Zhigang Wen</Link> as
-        a weekend project. As immigrants to the US, we had made many mistakes
-        when creating our first resumes and applying for internships and jobs.
-        It took us a long while to learn some of the best practices. While
-        mentoring first generation students and reviewing their resumes, we
-        noticed students were making the same mistakes that we had made before.
-        This led us to think about how we can be of help with the knowledge and
-        skills we have gained. We started chatting and working over the weekends
-        that led to OpenResume, where we integrated best practices and our
-        knowledge into this resume builder. Our hope is that OpenResume can help
-        anyone to easily create a modern professional resume that follows best
-        practices and enable anyone to apply for jobs with confidence.
+        OpenResume由<Link href="https://github.com/xitanggg">赵希堂</Link>开发，并由<Link href="https://www.linkedin.com/in/imzhi">文志刚</Link>设计，作为周末项目诞生。作为赴美的移民，我们在创建自己的第一份简历以及申请实习和工作时犯过许多错误。我们花了很长时间学习一些最佳实践。在指导第一代学生并审查他们的简历时，我们发现学生们正在犯我们以前同样的错误。这让我们思考如何利用所学知识和技能去帮助他们。于是，我们在周末开始交流和合作，最终形成了OpenResume，在其中融入了最佳实践和我们的知识。我们希望OpenResume可以帮助任何人轻松创建遵循最佳实践的现代专业简历，使任何人都能自信地申请工作。
       </p>
     ),
   },
   {
-    question: "Q4. How can I support OpenResume?",
+    question: "Q4. 如何支持OpenResume？",
     answer: (
       <>
         <p>
-          The best way to support OpenResume is to share your thoughts and
-          feedback with us to help further improve it. You can send us an email
-          at{" "}
-          <Link href="mailto:hello@open-resume.com">hello@open-resume.com</Link>{" "}
-          or{" "}
-          <Link href="https://github.com/xitanggg/open-resume/issues/new">
-            open an issue
-          </Link>{" "}
-          at our Github repository. Whether you like it or not, we would love to
-          hear from you.
+          支持OpenResume的最佳方式是向我们分享你的想法和反馈，帮助我们进一步改进产品。你可以通过邮件联系我们：<Link href="mailto:hello@open-resume.com">hello@open-resume.com</Link>，或者在我们的Github仓库中<Link href="https://github.com/xitanggg/open-resume/issues/new">
+            创建一个新的问题
+          </Link>。无论你喜欢还是不喜欢，我们都渴望听到你的声音。
         </p>
         <p>
-          Another great way to support OpenResume is by spreading the words.
-          Share it with your friends, on social media platforms, or with your
-          school’s career center. Our goal is to reach more people who struggle
-          with creating their resume, and your word-of-mouth support would be
-          greatly appreciated. If you use Github, you can also show your support
-          by{" "}
-          <Link href="https://github.com/xitanggg/open-resume">
-            giving the project a star
-          </Link>{" "}
-          to help increase its popularity and reach.
+          另一个支持OpenResume的好方法是口耳相传。将它分享给你的朋友、社交媒体平台，或者告诉你们学校的就业服务中心。我们的目标是让更多的简历制作有困难的人了解这个工具，所以你的口碑支持将非常宝贵。如果你使用Github，还可以通过<Link href="https://github.com/xitanggg/open-resume">
+            给该项目点赞（star）
+          </Link>来展示你的支持，这有助于提高项目的知名度和影响力。
         </p>
       </>
     ),
