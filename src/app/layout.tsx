2c9f4c75b16ff65bb7f3b452dfa8d68ce458f35d
Miro <45430033+HotelEnglish@users.noreply.github.com>
2024-02-22 16:53:18 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,9 +3,9 @@ import { TopNavBar } from "components/TopNavBar";
 import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
-  title: "OpenResume - Free Open-source Resume Builder and Parser",
+  title: "OpenResume - 免费开源简历生成器与解析器",
   description:
-    "OpenResume is a free, open-source, and powerful resume builder that allows anyone to create a modern professional resume in 3 simple steps. For those who have an existing resume, OpenResume also provides a resume parser to help test and confirm its ATS readability.",
+    "OpenResume是一款免费、开源且功能强大的简历生成器，它允许任何人只需简单3步即可创建一份现代专业简历。对于已有简历的人士，OpenResume还提供了简历解析器以帮助测试和确认其在ATS（招聘管理系统）中的可读性。",
 };
 
 export default function RootLayout({
