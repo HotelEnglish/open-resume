7fe5b88a73f0e5a3bc7d59f97d4faf85efa55da7
Miro <45430033+HotelEnglish@users.noreply.github.com>
2024-02-22 16:41:07 +0800

    Update constants.ts

--- src/app/home/constants.ts
+++ src/app/home/constants.ts
@@ -8,65 +8,65 @@ import type { Resume } from "lib/redux/types";
 import { deepClone } from "lib/deep-clone";
 
 export const END_HOME_RESUME: Resume = {
-  profile: {
-    name: "John Doe",
+   profile: {
+    name: "约翰·杜伊",
     summary:
-      "Software engineer obsessed with building exceptional products that people love",
+      "痴迷于构建卓越产品、深受人们喜爱的软件工程师",
     email: "hello@openresume.com",
     phone: "123-456-7890",
-    location: "NYC, NY",
+    location: "纽约州 纽约市",
     url: "linkedin.com/in/john-doe",
   },
   workExperiences: [
     {
-      company: "ABC Company",
-      jobTitle: "Software Engineer",
-      date: "May 2023 - Present",
+      company: "ABC公司",
+      jobTitle: "软件工程师",
+      date: "2023年5月 - 至今",
       descriptions: [
-        "Lead a cross-functional team of 5 engineers in developing a search bar, which enables thousands of daily active users to search content across the entire platform",
-        "Create stunning home page product demo animations that drives up sign up rate by 20%",
-        "Write clean code that is modular and easy to maintain while ensuring 100% test coverage",
+        "带领一个由5名工程师组成的跨职能团队开发搜索栏，使数千名日活跃用户能够在整个平台上搜索内容",
+        "创建吸引人的主页产品演示动画，从而提高注册率20%",
+        "编写模块化且易于维护的干净代码，同时确保100%的测试覆盖率",
       ],
     },
     {
-      company: "DEF Organization",
-      jobTitle: "Software Engineer Intern",
-      date: "Summer 2022",
+      company: "DEF组织",
+      jobTitle: "软件工程师实习生",
+      date: "2022年夏季",
       descriptions: [
-        "Re-architected the existing content editor to be mobile responsive that led to a 10% increase in mobile user engagement",
-        "Created a progress bar to help users track progress that drove up user retention by 15%",
-        "Discovered and fixed 5 bugs in the existing codebase to enhance user experience",
+        "重新架构现有的内容编辑器以实现手机响应式设计，导致移动用户参与度提高10%",
+        "创建进度条帮助用户跟踪进度，从而提高了用户留存率15%",
+        "发现并修复现有代码库中的5个bug，以提升用户体验",
       ],
     },
     {
-      company: "XYZ University",
-      jobTitle: "Research Assistant",
-      date: "Summer 2021",
+      company: "XYZ大学",
+      jobTitle: "研究助理",
+      date: "2021年夏季",
       descriptions: [
-        "Devised a new NLP algorithm in text classification that results in 10% accuracy increase",
-        "Compiled and presented research findings to a group of 20+ faculty and students",
+        "设计了一种新的文本分类NLP算法，使准确率提高10%",
+        "向20多名教职员工和学生汇编并展示研究成果",
       ],
     },
   ],
   educations: [
     {
-      school: "XYZ University",
-      degree: "Bachelor of Science in Computer Science",
-      date: "Sep 2019 - May 2023",
+      school: "XYZ大学",
+      degree: "计算机科学理学学士",
+      date: "2019年9月 - 2023年5月",
       gpa: "3.8",
       descriptions: [
-        "Won 1st place in 2022 Education Hackathon, 2nd place in 2023 Health Tech Competition",
-        "Teaching Assistant for Programming for the Web (2022 - 2023)",
-        "Coursework: Object-Oriented Programming (A+), Programming for the Web (A+), Cloud Computing (A), Introduction to Machine Learning (A-), Algorithms Analysis (A-)",
+        "在2022年教育黑客马拉松中荣获第一名，在2023年健康科技竞赛中获得第二名",
+        "担任《网页编程》（2022年至2023年）课程的教学助理",
+        "课程学习：面向对象编程（A+）、网页编程（A+）、云计算（A）、机器学习入门（A-）、算法分析（A-）",
       ],
     },
   ],
   projects: [
     {
       project: "OpenResume",
-      date: "Spring 2023",
+      date: "2023年春季",
       descriptions: [
-        "Created and launched a free resume builder web app that allows thousands of users to create professional resume easily and land their dream jobs",
+        "创建并推出一款免费简历生成器Web应用，帮助数以千计的用户轻松制作专业简历，并找到理想的工作",
       ],
     },
   ],
@@ -80,10 +80,10 @@ export const END_HOME_RESUME: Resume = {
       { skill: "C++", rating: 2 },
     ],
     descriptions: [
-      "Tech: React Hooks, GraphQL, Node.js, SQL, Postgres, NoSql, Redis, REST API, Git",
-      "Soft: Teamwork, Creative Problem Solving, Communication, Learning Mindset, Agile",
+      "技术栈：React Hooks、GraphQL、Node.js、SQL、Postgres、NoSql、Redis、REST API、Git",
+      "软技能：团队合作、创造性问题解决、沟通能力、学习心态、敏捷开发",
     ],
-  },
+  }, 
   custom: {
     descriptions: [],
   },
